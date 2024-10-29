47013bd257693885dd7f93f63cbad227e5a1e8df
susususushi <ssb200105@gamil.com>
2024-10-29 18:21:46 +0800

    Clear all files from main branch

--- README.md
+++
@@ -1,3 +0,0 @@
-# MLG_OCIL
-A Masking, Linkage and Guidance Framework for Online Class incremental Learning.  
-The code will be updated soon.
--- models/pretrained.py
+++
@@ -1,7 +0,0 @@
-import torchvision.models as models
-import torch
-
-def ResNet18_pretrained(n_classes):
-    classifier = models.resnet18(pretrained=True)
-    classifier.fc = torch.nn.Linear(512, n_classes)
-    return classifier
